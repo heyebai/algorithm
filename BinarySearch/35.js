a96e16c4a3fcda297fb8e16c4fe426a1c96e5c39
heyebai <1553792549@qq.com>
2022-03-06 17:07:27 +1000

    update

---
+++ BinarySearch/35.js
@@ -0,0 +1,28 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let l = 0,
+        r = nums.length - 1;
+
+    while (l <= r) {
+        mid = Math.floor((l + r) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    if (nums[mid] > target) {
+        return mid;
+    }
+    return mid + 1;
+};
