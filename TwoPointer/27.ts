f8ad01f9cac6b91f59838e306b0537ceec0d913f
heyebai <1553792549@qq.com>
2022-04-05 22:48:37 +1000

    27

---
+++ TwoPointer/27.ts
@@ -0,0 +1,10 @@
+function removeElement(nums: number[], val: number): number {
+  let slow: number = 0;
+  nums.forEach((num, index) => {
+    if (num !== val) {
+      nums[slow++] = nums[index];
+    }
+  });
+  return slow;
+}
+g;
