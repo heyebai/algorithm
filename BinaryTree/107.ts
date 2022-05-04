bf6b785ea1bc11ba189f8968697502cd867a48bc
heyebai <1553792549@qq.com>
2022-05-05 00:49:15 +1000

    107

---
+++ BinaryTree/107.ts
@@ -0,0 +1,25 @@
+import TreeNode from "./treeNode";
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+  if (!root) {
+    return [];
+  }
+  const queue: TreeNode[] = [root];
+  const result: number[][] = [];
+  while (queue.length) {
+    const len = queue.length;
+    const layer: number[] = [];
+    for (let i = 0; i < len; i++) {
+      const node: TreeNode = queue.shift();
+      layer.push(node.val);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    result.push(layer);
+  }
+  return result.reverse();
+}
