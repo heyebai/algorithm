f4af2ec0fd040f5851ce83ef74972b65e6451898
heyebai <1553792549@qq.com>
2022-05-10 00:34:54 +1000

    617

---
+++ BinaryTree/617.ts
@@ -0,0 +1,24 @@
+import TreeNode from "./treeNode";
+
+function mergeTrees(
+  root1: TreeNode | null,
+  root2: TreeNode | null
+): TreeNode | null {
+  let node: TreeNode | null;
+  if (!root1 && !root2) {
+    node = null;
+  } else if (!root1 && root2) {
+    node = root2;
+    node.left = mergeTrees(null, root2.left);
+    node.right = mergeTrees(null, root2.right);
+  } else if (root1 && !root2) {
+    node = root1;
+    node.left = mergeTrees(root1.left, null);
+    node.right = mergeTrees(root1.right, null);
+  } else {
+    node = new TreeNode(root1.val + root2.val);
+    node.left = mergeTrees(root1.left, root2.left);
+    node.right = mergeTrees(root1.right, root2.right);
+  }
+  return node;
+}
