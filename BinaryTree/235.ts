8301f61e8846de21d4029beb7c38566807026452
heyebai <1553792549@qq.com>
2022-05-15 10:18:45 +1000

    235

---
+++ BinaryTree/235.ts
@@ -0,0 +1,17 @@
+import TreeNode from "./treeNode";
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
+  if (!root || !p || !q) {
+    return null;
+  }
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+  return root;
+}
