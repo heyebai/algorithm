6d4ba432fab875b21731b3eea980c5ac7ce8ba2f
heyebai <1553792549@qq.com>
2022-06-12 13:29:03 +1000

    377

---
+++ DP/377.ts
@@ -0,0 +1,13 @@
+function combinationSum4(nums: number[], target: number): number {
+  const dp = new Array(target + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 0; i <= target; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i >= nums[j]) {
+        dp[i] += dp[i - nums[j]];
+      }
+    }
+  }
+  return dp[target];
+}
