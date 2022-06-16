fa30708a43078468bd167b28bb330b125fd2c0d0
heyebai <1553792549@qq.com>
2022-06-16 21:56:27 +1000

    122

---
+++ BinaryTree/122.ts
@@ -0,0 +1,21 @@
+function maxProfit(prices: number[]): number {
+  let hold = prices[0],
+    profit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > hold) {
+      profit += prices[i] - hold;
+    }
+    hold = prices[i];
+  }
+  return profit;
+}
+
+function maxProfit1(prices: number[]): number {
+  const dp = new Array(prices.length).fill(0).map(() => new Array(2).fill(0));
+  dp[0][0] = -prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+  }
+  return dp[prices.length - 1][1];
+}
