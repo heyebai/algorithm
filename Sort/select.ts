80437b06f0bf06d8bf467e84009b8f997c184899
heyebai <1553792549@qq.com>
2022-06-23 22:58:29 +1000

    select

---
+++ Sort/select.ts
@@ -0,0 +1,16 @@
+export const arr = [1, 3, 29, 19, 9, 2, 4, 33, 6, 8];
+
+const select = () => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  return arr;
+};
