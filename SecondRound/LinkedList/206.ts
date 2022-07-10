23c5e72b49c4b8ed62f25ad928131c86f2eb02c1
heyebai <1553792549@qq.com>
2022-07-10 15:02:46 +1000

    206

---
+++ SecondRound/LinkedList/206.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head;
+  let curr = head;
+  let next = null;
+  let prev = null;
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
