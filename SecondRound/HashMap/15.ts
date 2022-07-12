1f78558935fbc121d0ca33eddd3256ad81b70f28
heyebai <1553792549@qq.com>
2022-07-12 21:51:36 +1000

    15

---
+++ SecondRound/HashMap/15.ts
@@ -0,0 +1,29 @@
+function threeSum(nums: number[]): number[][] {
+  nums.sort((a, b) => a - b);
+  const res: number[][] = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let left = i + 1,
+      right = nums.length - 1;
+    while (left < right) {
+      if (nums[i] + nums[left] + nums[right] === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        while (nums[left] === nums[left - 1]) {
+          left++;
+        }
+        while (nums[right] === nums[right + 1]) {
+          right--;
+        }
+      } else if (nums[i] + nums[left] + nums[right] > 0) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+  return res;
+}
--- SecondRound/Untitled-1.ts
+++ SecondRound/Untitled-1.ts
@@ -1,6 +1,7 @@
-let store = [1, 2, 3, 4, 5, 6];
+let store = [1, 2, 7, 4, 5, 6];
 
 store = store.slice(0, 1).concat(store.slice(1 + 1));
 
 const a = { 2: 3 };
 console.log(a[3]);
+console.log(store.sort((a, b) => (a - b > 0 ? 1 : -1)));
