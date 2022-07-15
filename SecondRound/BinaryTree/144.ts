352d2103733c91dc031e9d55b6ddba96b4b93497
heyebai <1553792549@qq.com>
2022-07-16 04:06:04 +1000

    144

--- SecondRound/BinaryTree/144.ts
+++ SecondRound/BinaryTree/144.ts
@@ -22,3 +22,31 @@ function preorderTraversal(root: TreeNode | null): number[] {
   helper(root, res);
   return res;
 }
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+  if (!root) return [];
+  const stack = [];
+  const res = [];
+  stack.push(root);
+  while (stack.length) {
+    const node = stack.pop();
+    res.push(node.val);
+    node.right && stack.push(node.right);
+    node.left && stack.push(node.left);
+  }
+  return res;
+}
