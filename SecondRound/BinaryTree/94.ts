c685298df18d89aebb0061b1cddc64be4590ea16
heyebai <1553792549@qq.com>
2022-07-16 13:43:14 +1000

    94

--- SecondRound/BinaryTree/94.ts
+++ SecondRound/BinaryTree/94.ts
@@ -22,3 +22,35 @@ function inorderTraversal(root: TreeNode | null): number[] {
   helper(root, res);
   return res;
 }
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  if (!root) return [];
+  const stack = [];
+  const res = [];
+  let curr = root;
+  while (curr || stack.length) {
+    if (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    } else {
+      const node = stack.pop();
+      res.push(node.val);
+      curr = node.right;
+    }
+  }
+  return res;
+}
