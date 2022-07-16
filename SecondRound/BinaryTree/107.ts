a3ff20942561b7dabfd657edfdb821ac40640d4b
heyebai <1553792549@qq.com>
2022-07-16 14:00:48 +1000

    107

---
+++ SecondRound/BinaryTree/107.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+  if (!root) return [];
+  const arr = [];
+  const res = [];
+  arr.push(root);
+  while (arr.length) {
+    const len = arr.length;
+    const temp = [];
+    for (let i = 0; i < len; i++) {
+      const node = arr.shift();
+      temp.push(node.val);
+      node.left && arr.push(node.left);
+      node.right && arr.push(node.right);
+    }
+    res.push(temp);
+  }
+  return res.reverse();
+}
