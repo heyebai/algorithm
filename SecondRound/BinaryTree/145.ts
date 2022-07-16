40ccc7b7c0a2c968eec9a10ca2ab178e36a42165
heyebai <1553792549@qq.com>
2022-07-16 12:05:29 +1000

    145

--- SecondRound/BinaryTree/145.ts
+++ SecondRound/BinaryTree/145.ts
@@ -22,3 +22,31 @@ function postorderTraversal(root: TreeNode | null): number[] {
   helper(root, res);
   return res;
 }
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+  if (!root) return [];
+  const stack = [];
+  stack.push(root);
+  const res = [];
+  while (stack.length) {
+    const node = stack.pop();
+    res.push(node.val);
+    node.left && stack.push(node.left);
+    node.right && stack.push(node.right);
+  }
+  return res.reverse();
+}
