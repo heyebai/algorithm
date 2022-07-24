1692ca6e25377c72075b371ee7f8c7915700cd4d
heyebai <1553792549@qq.com>
2022-07-24 21:31:24 +1000

    121

---
+++ SecondRound/DP/121.ts
@@ -0,0 +1,11 @@
+function maxProfit(prices: number[]): number {
+  const dp: number[][] = new Array(prices.length)
+    .fill(0)
+    .map((_) => new Array(2).fill(0));
+  dp[0][0] = -prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
+  }
+  return dp[prices.length - 1][1];
+}
