f6a9c895341bba6e6feb14e5f383918a0b078478
heyebai <1553792549@qq.com>
2022-08-16 01:21:01 +1000

    538

---
+++ thirdRound/Tree/538.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function convertBST(root: TreeNode | null): TreeNode | null {
+  let sum = 0;
+  const helper = (node: TreeNode | null) => {
+    if (node === null) return;
+    helper(node.right);
+    node.val = node.val + sum;
+    sum = node.val;
+    helper(node.left);
+  };
+  helper(root);
+  return root;
+}
