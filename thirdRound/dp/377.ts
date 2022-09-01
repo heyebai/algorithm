f09972ac81eff49c21bbff538ac1ce4b20f9ee27
heyebai <1553792549@qq.com>
2022-09-02 00:20:39 +1000

    377

---
+++ thirdRound/dp/377.ts
@@ -0,0 +1,12 @@
+function combinationSum4(nums: number[], target: number): number {
+  const dp: number[] = new Array(target + 1).fill(0);
+  dp[0] = 1;
+  for (let j = 0; j <= target; j++) {
+    for (let i = 0; i < nums.length; i++) {
+      if (j >= nums[i]) {
+        dp[j] += dp[j - nums[i]];
+      }
+    }
+  }
+  return dp[target];
+}
