391403aa2557716a9496ceb2192a25565c1b7846
heyebai <1553792549@qq.com>
2022-09-08 00:39:26 +1000

    122

---
+++ thirdRound/dp/122.ts
@@ -0,0 +1,12 @@
+function maxProfit(prices: number[]): number {
+  const dp: number[][] = new Array(prices.length)
+    .fill(0)
+    .map((_) => new Array(2).fill(0));
+  // 0:hold stock 1: not hold a stock
+  dp[0][0] = -prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+  }
+  return dp[prices.length - 1][1];
+}
