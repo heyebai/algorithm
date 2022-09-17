c1373b48c811ffbefe54fd2fef4c0914d3815a96
heyebai <1553792549@qq.com>
2022-09-17 14:48:49 +1000

    1143

---
+++ thirdRound/dp/1143.ts
@@ -0,0 +1,17 @@
+function longestCommonSubsequence(text1: string, text2: string): number {
+  const dp: number[][] = new Array(text1.length + 1)
+    .fill(0)
+    .map((_) => new Array(text2.length + 1).fill(0));
+  let max = 0;
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+      max = Math.max(max, dp[i][j]);
+    }
+  }
+  return max;
+}
